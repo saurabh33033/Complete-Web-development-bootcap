218a5c3eefae58f867803ab0c103e9332c240e6d
saurabh <saurabh@trapkiller.com>
2021-04-15 12:47:54 +0530

    Added index & js/ as tut2

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.time("your code took");
+console.log("Hello Console");
+console.log(43434*654651);
+console.log(true);
+console.log([3332, 334, 234, 234, 324, 232]);
+console.log({name:"Saurabh Gupta", mark: "out of", pet: "Rahul"});
+console.table({name:"Saurabh Gupta", mark: "out of", pet: "Rahul"});
+console.warn("Rahul is escap form jungle");
+// console.clear();
+console.timeEnd("your code took");
+console.assert(13>18, "Age > 18 not aloud");
+console.error("This is an error 4o4")
+
+/*
+multi
+line
+comment
+*/
