6962d58cf08a7520b42e02ff8d60b006b85559e9
saurabh <saurabh@trapkiller.com>
2021-04-15 15:08:03 +0530

    Added tut3.js as tut3

---
+++ js/tut3.js
@@ -0,0 +1,42 @@
+console.log("tut3")
+// Variables in js
+// var, let, const
+
+var name = "Saurabh";
+var channel;
+channel = "TrapKiller";
+mark = 3400045;
+
+console.log(name, channel, mark);
+
+/* Rules for creating Java Script Variables
+1. Cannot not start with Number
+2. Can start with letter, Number, _ or $
+3. Java Script Variables are case sensitive
+*/
+var city = "Mumbai";
+console.log(city);
+
+const ownerName = "Hari Ram";
+console.log(ownerName);
+
+{
+    const arr1 = 34;
+    console.log(arr1)
+    let city = "Rampur";
+    city = "Delhi"
+    console.log(city);
+}
+
+console.log(city);
+
+const arr1 = [23, 33, 43, 87,];
+// arr1.push(344);
+console.log(arr1);
+
+/* Most Common Programming Case Type:
+1. camelCase
+2. kabab-case
+3. snake_case
+4. PascalCase
+*/
