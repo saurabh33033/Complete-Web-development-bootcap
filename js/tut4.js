6f13b194401bdbd018ac352b0f36e4bb2bf20c46
saurabh <saurabh@trapkiller.com>
2021-04-15 16:00:06 +0530

    Added tut4.js as tut4

---
+++ js/tut4.js
@@ -0,0 +1,53 @@
+//Primitive Data types:
+
+//String
+let name = "Saurabh";
+console.log("My String value is " + name);
+console.log("My Data type is " + (typeof name));
+
+//Numbers
+let mark = 34.4;
+console.log("My Number value is " + mark);
+console.log("My Data type is " + (typeof mark));
+
+//Boolean
+let isDriver = true;
+console.log("My Boolean value is " + isDriver);
+console.log("My Data type is " + (typeof isDriver));
+
+//Null
+let nullVar = null;
+console.log("My Null value is " + nullVar);
+console.log("My Data type is " + (typeof nullVar));
+
+//Undefined
+let undef = undefined;
+console.log("My Undefined value is " + undef);
+console.log("My Data type is " + (typeof undef));
+
+
+//Reference Data types
+
+//Arrays
+let myArr = [1, 2, 3, 4, 5];
+console.log("My Data type is " + (typeof myArr));
+
+//Object Literals
+let marks = {
+    Saurabh: 100,
+    Sumit: 60,
+    Affan: 80,
+    Rahul: "Kuta"
+};
+console.log(marks);
+console.log("My Data type is " + (typeof marks));
+
+//Function
+function findName() {
+    
+};
+console.log("My Data type is " + (typeof findName));
+
+//Dates
+let date = new Date ();
+console.log("My Data type is " + (typeof date));
