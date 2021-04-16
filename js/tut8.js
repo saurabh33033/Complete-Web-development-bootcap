1e3d36633ba9442e14a0cdfecc8f78258edb7646
saurabh <saurabh@trapkiller.com>
2021-04-16 21:00:33 +0530

    Added tut8.js as tut8

---
+++ js/tut8.js
@@ -0,0 +1,45 @@
+console.log("We are at tut8");
+const age = 54;
+const vari = 34;
+const doesDrive = true;
+
+// if(age!=19){
+//     console.log('Age is not 19');
+// }
+// if(age !== 65){
+//    console.log('Age is not 65');
+// }
+// else{
+//     console.log('Age is not 19');
+// }
+
+// if(typeof vari !== undefined){
+//     console.log('Vari is defined');
+// }
+// else{
+//     console.log('Vari is undefined');
+// }
+
+// if(doesDrive || age>17){
+//     console.log('You can Drive');
+// }
+// else{
+//     console.log('You cannot Drive');
+// }
+
+// console.log(age==54? 'Age is 54': 'Age is not 54');
+
+switch (age) {
+    case 18:
+        console.log('You are 18');
+        break;
+    case 28:
+        console.log('You are 18');
+        break;
+    case 38:
+        console.log('You are 18');
+        break;      
+    default:
+        console.log('Failed to find age');
+        break;
+}
