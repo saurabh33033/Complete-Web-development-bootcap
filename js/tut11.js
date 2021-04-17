0cf30554c256ea6b2234b59536ed58541b51b0e7
saurabh <saurabh@trapkiller.com>
2021-04-17 22:20:56 +0530

    Added tut11.js as tutorial 11

---
+++ js/tut11.js
@@ -0,0 +1,15 @@
+console.log('This is tut11');
+let a = window.document;
+// // a = prompt('This will destroy your computer. Type your name.');
+// a = confirm('You want to delete this page.');
+// a = window.innerHeight;
+// a = window.innerWidth;
+// a = scrollX;
+// a = scrollY;
+a = location;
+// a = location.reload();
+// a = location.href;
+// a = location.href = 'facebook.com';
+// a = location.href = '//facebook.com';
+// a = location.toString();
+console.log(a);
