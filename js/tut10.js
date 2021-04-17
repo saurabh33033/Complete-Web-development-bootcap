082926a2a81358dd367000132f99a6d9dea7fa1c
saurabh <saurabh@trapkiller.com>
2021-04-17 19:24:03 +0530

    Added tut10.js as tutorial 10

---
+++ js/tut10.js
@@ -0,0 +1,31 @@
+console.log('We are at tutoraial 10');
+let i = 234;
+console.log(i);
+function ui(name){
+    return `This is ${name} ui`
+}
+console.log(ui("Saurabh"))
+
+// const myGreet = function(name, thank) {
+//     let msg = `Happy Birth Day ${name} Live your life with smiles, not tears. Beat your age with friends and not years. ${thank}!`;
+//     return msg;
+// };
+// let name = 'SkillF';
+// let name2 = 'Rohan';
+
+// let val = myGreet(name, 'Thank a lot');
+// console.log(val);
+
+// const myObj = {
+//     name: 'Saurabh',
+//     game: function(){
+//         return 'GTA';
+//     }
+// }
+// console.log(myObj.game());
+
+// arr = ["fruits", "vegetable", "furniture"];
+
+// arr.forEach(function(element , index, array) {
+//     console.log(element, index);
+// });
