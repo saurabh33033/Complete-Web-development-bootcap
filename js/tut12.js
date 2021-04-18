2df9441503ae9d7c866264df441d0ae80d7a1656
saurabh <saurabh@trapkiller.com>
2021-04-18 16:54:13 +0530

    Added tut12.js as Tutorial 12

---
+++ js/tut12.js
@@ -0,0 +1,20 @@
+console.log('Welcome to tut12.js');
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms;
+// Array.from(a).forEach(function(element) {
+//     console.log(element);
+// });
+// a = document.links;
+a = document.links[0];
+// a = document.links[0].href;
+console.log(a);
+
+//Quick Quiz
+//use document.images and document.scripts and print the list of images and scripts on html page.
+
+// let quizImg = document.images;
+// let quizSrc = document.scripts;
+// console.log(quizImg);
+// console.log(quizSrc);
