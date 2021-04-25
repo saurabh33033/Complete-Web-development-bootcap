fe943026e2a2ebf19dec4d909de9f5449de7aa84
saurabh <saurabh@trapkiller.com>
2021-04-25 16:44:37 +0530

    Added tut15.js as tutorial 15

---
+++ js/tut15.js
@@ -0,0 +1,31 @@
+console.log('Welcome to tutorial 15');
+
+let cont = document.querySelector('.no');
+cont = document.querySelector('.container');
+let nodeName = cont.childNodes[0].nodeName;
+let nodeType = cont.childNodes[0].nodeType;
+// console.log(nodeType);
+// console.log(nodeName);
+
+/* Node Types 
+1. Element
+2. Attribute
+3. Text Node
+8. Comment
+9. Document
+10. docType
+*/
+
+// console.log(cont.childNodes);
+// console.log(cont.children);
+
+let container = document.querySelector('.container');
+// console.log(container.children[1].children[0]);
+// console.log(container.firstChild);
+// console.log(container.firstElementChild);
+// console.log(container.lastChild);
+// console.log(container.lastElementChild);
+// console.log(container.childElementCount);
+// console.log(container.firstElementChild.parentNode);
+// console.log(container.firstElementChild.parentNode.nextElementSibling);
+// console.log(container.firstElementChild.parentNode.nextSibling);
